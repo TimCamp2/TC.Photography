3d3f2e80a1b7881aabaed3e530471b0c1838084a
TimCamp2 <timcamp2@gmail.com>
2014-03-24 23:29:35 -0400

    Created main divs, fixed horizontal alignment

--- slider.js
+++ slider.js
@@ -1,7 +1,6 @@
 function Slider(container, nav) {
     this.container = container;
     this.nav = nav.show();
-
     this.imgs = this.container.find('img');
     this.imgWidth = this.imgs[0].width; // 600
     this.imgsLen = this.imgs.length;
