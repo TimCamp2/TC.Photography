22964e63c5c16de980cee1391031372fdba801a2
TimCamp2 <timcamp2@gmail.com>
2014-04-28 17:03:23 -0400

    Updated jquery effects

--- js/picSize.js
+++ js/picSize.js
@@ -1,10 +1,10 @@
 $('.img-thumbnail').hover(makeBigger, returnToOrgin);
 
         function makeBigger() {
-            $(this).animate().css({height: '+=10%', width: '+=10%', opacity: '0.6'});
-        }
+            $(this).animate().css({opacity: '0.6'});
+       }
 
         function returnToOrgin() {
-            $(this).animate().css({height: "", width: "", opacity: ""});
+            $(this).animate().css({opacity: ""});
         }
  
