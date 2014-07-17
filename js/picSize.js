6068887831d45ae8342f3bcd6fe1ca2ccf523ac5
TimCamp2 <timcamp2@gmail.com>
2014-07-16 23:03:26 -0400

    Made images smaller+prelim scroll feature

--- js/picSize.js
+++ js/picSize.js
@@ -8,3 +8,16 @@ $('.img-thumbnail').hover(makeBigger, returnToOrgin);
             $(this).animate().css({opacity: ""});
         }
  
+
+        $('.box').hover(
+            function() {
+                $(this).addClass('slider');
+            },
+            function() {
+                $(this).removeClass('slider');
+            }
+);
+
+//Two changes need to be made, 1st- borders from added class are 
+//pushing rest of page downwards. 2nd- need to figure out how to make
+//added class slide down upon hover AND not disappear.
